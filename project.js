a0e4952a56b236e5fcc3204b1447a1c39a5114ea
Роман bc2024 <kernitsky06@gmail.com>
2024-10-07 09:12:08 +0300

    Редагування коду для хорошого вигляду

--- project.js
+++ project.js
@@ -9,28 +9,30 @@ program
 program.parse();
 
 const options = program.opts();
+
 if (!options.input) {
     console.error("Please, specify input file");
 }
+
 fs.readFile(options.input, 'utf8', (err, data) => {
-    if (err) {
-      console.error("Cannot find input file");
-      process.exit(1);
-    }
-    const formattedResult = JSON.stringify(JSON.parse(data), null, 2);
-    if (options.display) {
-      console.log(formattedResult);
-    }
-    if (options.output) {
-        fs.writeFile(options.output, formattedResult, 'utf8', (writeErr) => {
-            if (writeErr) {
-              process.exit(1); // Завершення програми при помилці без виводу повідомлення
-            }
-          });
-        }
-        if (options.output2) {
-            for (let i of JSON.parse(data)){
-                fs.appendFileSync(options.output2, `${i.StockCode}-${i.ValCode}-${i.Attraction}\n`, 'utf8')
-            }
+  if (err) {
+    console.error("Cannot find input file");
+    process.exit(1);
+  }
+  const formattedResult = JSON.stringify(JSON.parse(data), null, 2);
+  if (options.display) {
+    console.log(formattedResult);
+  }
+  if (options.output) {
+      fs.writeFile(options.output, formattedResult, 'utf8', (writeErr) => {
+          if (writeErr) {
+            process.exit(1); // Завершення програми при помилці без виводу повідомлення
           }
-  });
+      });
+  }
+  if (options.output2) {
+      for (let i of JSON.parse(data)){
+          fs.appendFileSync(options.output2, `${i.StockCode}-${i.ValCode}-${i.Attraction}\n`, 'utf8')
+      }
+  }
+});
